cac78de8dc2879d10e27c4fcf4ac2568228d46be
Blog.apk <46821030+BuatApkOlshop@users.noreply.github.com>
2019-03-21 03:11:28 +0800

    Create Header.java

---
+++ Webock/Header.java
@@ -0,0 +1,9 @@
+Request URL: https://www.juggle-jack.com/store/bliblico
+Request method: POST
+content-type: application/x-www-form-urlencoded
+Expect: 
+User-Agent: GitHub-Hookshot/a0a6977
+X-GitHub-Delivery: 71bf8000-4b3b-11e9-9b70-213eecd736e0
+X-GitHub-Event: ping
+X-Hub-Signature: sha1=88e40f07fe7dae56e430f6ca98b5ed1196ded389
+
